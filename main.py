d52829d394e64fb5d0dc2fd0c00f734dfe58ece6
Juan Santiago <jjbueno93@gmail.com>
2025-10-04 13:32:29 +0200

    Primer commit del main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+#Declaro variables
+(width, height) = (800, 600)
+rectangulo = pygame.Rect(400, 300, 30, 50)
+screen = pygame.display.set_mode((width, height))
+#Inicializa pygame 
+pygame.init()
+pygame.display.set_caption("Juego de Vero")
+
+
+#Bucle del juego
+running = True
+while running:
+    #Captura de eventos
+    events = pygame.event.get()
+    #Asigancion del evento closeWindow
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit ()
+
+
+    screen.fill((0,0,0))
+    
+    pygame.draw.rect(screen, (255,255,255), rectangulo)
+
+    pygame.display.flip()
+    
+       
