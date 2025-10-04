56c647bdc0af265e5017150f23d9c2603f978404
Juan Santiago <jjbueno93@gmail.com>
2025-10-04 17:08:50 +0200

    Ajuste de velocidad de las palas y limites de movimiento

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 #Declaro variables
 (width, height) = (1000, 800)
+screen = pygame.display.set_mode((width, height))
 pala1 = pygame.Rect(150, 300, 15, 60)
 pala2 = pygame.Rect(850, 300, 15, 60)
-screen = pygame.display.set_mode((width, height))
+ball_center = (500, 400)
+pala1_y_real = pala1.y
+pala2_y_real = pala2.y
+velocidad = 0.2
 #Inicializa pygame 
 pygame.init()
 pygame.display.set_caption("Juego de Vero")
@@ -20,24 +24,41 @@ while running:
             running = False
             pygame.quit ()
 
-    #Registrar teclas pulsadas
+    #Registrar teclas pulsadas y mover las palas
     keys_state = pygame.key.get_pressed()
+
     if keys_state[pygame.K_w]:
-        pala1.y -= 1
+        pala1_y_real -= velocidad
     elif keys_state[pygame.K_s]:
-        pala1.y += 1
+        pala1_y_real += velocidad
 
     if keys_state[pygame.K_UP]:
-        pala2.y -= 1
+        pala2_y_real -= velocidad
     elif keys_state[pygame.K_DOWN]:
-        pala2.y += 1
-    
+        pala2_y_real += velocidad
+    #Limitar el movimiento de las palas a los bordes 
+    if pala1_y_real < 0:
+        pala1_y_real = 0
+    if pala1_y_real > height - pala1.height:
+        pala1_y_real = height - pala1.height
+
+    if pala2_y_real < 0:
+        pala2_y_real = 0
+    if pala2_y_real > height - pala2.height:
+        pala2_y_real = height - pala2.height
+    #Actualizar la posicion de y mediante una variable
+    # para poder aumentar y disminuir en decimales
+    pala1.y = int(pala1_y_real)
+    pala2.y = int(pala2_y_real)
+
 
     #Pinta un fondo nuevo
     screen.fill((0,0,0))
 
+    #Dibuja las palas
     pygame.draw.rect(screen, (255,255,255), pala1)
     pygame.draw.rect(screen, (255,255,255), pala2)
+    pygame.draw.circle(screen, (255,255,255), ball_center, 10)
 
     #Actualiza la screen
     pygame.display.flip()
