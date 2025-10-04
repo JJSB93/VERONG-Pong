32f49fd580ef0b5c28771ab095e3fd59bbc0978e
Juan Santiago <jjbueno93@gmail.com>
2025-10-04 16:00:25 +0200

    Palas y movimiento con teclas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 #Declaro variables
-(width, height) = (800, 600)
-rectangulo = pygame.Rect(400, 300, 30, 50)
+(width, height) = (1000, 800)
+pala1 = pygame.Rect(150, 300, 15, 60)
+pala2 = pygame.Rect(850, 300, 15, 60)
 screen = pygame.display.set_mode((width, height))
 #Inicializa pygame 
 pygame.init()
@@ -19,12 +20,28 @@ while running:
             running = False
             pygame.quit ()
 
+    #Registrar teclas pulsadas
+    keys_state = pygame.key.get_pressed()
+    if keys_state[pygame.K_w]:
+        pala1.y -= 1
+    elif keys_state[pygame.K_s]:
+        pala1.y += 1
+
+    if keys_state[pygame.K_UP]:
+        pala2.y -= 1
+    elif keys_state[pygame.K_DOWN]:
+        pala2.y += 1
+    
+
     #Pinta un fondo nuevo
     screen.fill((0,0,0))
 
-    pygame.draw.rect(screen, (255,255,255), rectangulo)
-    
+    pygame.draw.rect(screen, (255,255,255), pala1)
+    pygame.draw.rect(screen, (255,255,255), pala2)
+
     #Actualiza la screen
     pygame.display.flip()
-    
-       
+
+
+#Desinicializacion de pygame
+pygame.quit()
